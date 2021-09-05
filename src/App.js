583dc8e04e49dcb22e9999ddaa33de3a0e9309a2
Venturing Guy <LVentura335@Gmail.com>
2021-09-05 13:30:25 -0700

    removed Quantities, beginning new CoinCounter func

--- src/App.js
+++ src/App.js
@@ -1,24 +1,60 @@
-import './App.css';
-import Buttons from './Buttons';
-import Quantities from './Quantities';
-import Total from './Total';
+import './App.css'
+import Buttons from './Buttons'
+import Total from './Total'
+import { useState } from 'react'
+
+const COIN_TO_CURRENCY = [0.01, 0.05, 0.10, 0.25]
+const COIN_TO_LABEL = [['penny', 'pennies'], ['nickel', 'nickels'], ['dime', 'dimes'], ['quarter', 'quarters']]
+
+const CoinCounter = (props) => {
+  const {increaseCoin, label} = props
+  return <button onClick={increaseCoin}>
+    {label}
+   </button>
+}
 
 function App() {
-  return (
+  const [coinAmount, setCoins] = useState([0, 0, 0, 0]);
+  
+  const increaseCounter = (index) => {
+    const coinCopy = [...coinAmount];
+    coinCopy[index] += 1;
+    setCoins(coinCopy);
+  }
+
+  const renderCoinCounter = (label, handleIncrease) => (
+    <CoinCounter
+      increaseCoin={handleIncrease}
+      label={label}
+    />
+   )
+
+  const coinCounters = coinAmount.map((_, index) => {
+    const label = `$0.${COIN_TO_CURRENCY[index]}`;
+    const handleIncrease = () => increaseCounter(index);
+    return renderCoinCounter(label, handleIncrease);
+  });
+
+
+  const renderTotal = () => {
+    const runningTotal = coinAmount.reduce((acc, val, index) => {
+      return acc + (val * COIN_TO_CURRENCY[index]);
+     });
+    return <span>Total: ${runningTotal}</span>
+  }
+  
+    return (
     <div className="App">
       <h1>Coin Collector!</h1>
-      <Total
-        total="3"
-      />
-      <Quantities
-        pennies="1"
-        nickels="2"
-        dimes="3"
-        quarters="4"
-      />
+      
       <Buttons />
     </div>
   );
 }
+
+
+
+
+
 
 export default App;
