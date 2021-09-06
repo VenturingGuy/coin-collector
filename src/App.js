0e39a66f16c100cdacd731b1804bf850ded0eaee
Venturing Guy <LVentura335@Gmail.com>
2021-09-05 23:09:51 -0700

    removed whitespace, moved CoinCounter

--- src/App.js
+++ src/App.js
@@ -4,45 +4,47 @@ import { useState } from 'react'
 const COIN_TO_CURRENCY = [0.01, 0.05, 0.10, 0.25]
 const COIN_TO_LABEL = [['Penny', 'Pennies'], ['Nickel', 'Nickels'], ['Dime', 'Dimes'], ['Quarter', 'Quarters']]
 
-const CoinCounter = (props) => {
-  const {increaseCoin, label} = props
-  return <button onClick={increaseCoin}>
-    {label}
-   </button>
-}
 
 function App() {
   const [coinAmount, setCoins] = useState([0, 0, 0, 0]);
+
+  const CoinCounter = (props) => {
+    const {increaseCoin, label} = props
+    return <button onClick={increaseCoin}>
+      {label}
+     </button>
+  }
   
   const increaseCounter = (index) => {
-    const coinCopy = [...coinAmount];
-    coinCopy[index] += 1;
-    setCoins(coinCopy);
+    const coinCopy = [...coinAmount]
+    coinCopy[index] += 1
+    setCoins(coinCopy)
   }
 
   const renderCoinCounter = (label, handleIncrease) => (
     <CoinCounter
-      increaseCoin={handleIncrease}
       label={label}
+      increaseCoin={handleIncrease}
     />
    )
 
   const coinCounters = coinAmount.map((_, index) => {
     // toFixed ensures that the dime button is listed as $0.10 rather than $0.1
-    const label = `$${COIN_TO_CURRENCY[index].toFixed(2)}`;
-    const handleIncrease = () => increaseCounter(index);
-    return renderCoinCounter(label, handleIncrease);
+    const label = `$${COIN_TO_CURRENCY[index].toFixed(2)}`
+    const handleIncrease = () => increaseCounter(index)
+    return renderCoinCounter(label, handleIncrease)
   });
 
   const coinTotals = coinAmount.map((amount, index) => {
-    const coinLabels = (amount === 0 || amount > 1) ? COIN_TO_LABEL[index][1] : COIN_TO_LABEL[index][0];
+    // returns respective coinLabel (through index) as singular or plural based on quantity
+    const coinLabels = (amount === 0 || amount > 1) ? COIN_TO_LABEL[index][1] : COIN_TO_LABEL[index][0]
     return <span>{coinLabels}: {amount} </span>
   });
 
   const renderTotal = () => {
     const runningTotal = coinAmount.reduce((acc, val, index) => {
-      return acc + (val * COIN_TO_CURRENCY[index]);
-     });
+      return acc + (val * COIN_TO_CURRENCY[index])
+     })
     return <span>Total: ${runningTotal}</span>
   }
   
@@ -56,10 +58,5 @@ function App() {
     </div>
   );
 }
-
-
-
-
-
 
 export default App;
